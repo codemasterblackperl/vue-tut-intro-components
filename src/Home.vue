49c907acc612678cb17ef40b03c1db4a9cd1952e
Ajit <codemasterblackperl@outlook.com>
2018-05-17 22:48:40 +0530

    added global and local component

--- src/Home.vue
+++ src/Home.vue
@@ -1,21 +1,14 @@
 <template>
   <div>
-    <p>Server Status: {{status}}</p>
-    <hr>
-    <button @click="changeStatus">Change Status</button>
+    <app-server-status v-for="(server,index) in 5"
+                       :key="index"></app-server-status>
   </div>
 </template>
 <script>
+import ServerStatus from "./ServerStatus.vue";
 export default {
-  data: function() {
-    return {
-      status: "Critical"
-    };
-  },
-  methods: {
-    changeStatus() {
-      this.status = "Normal";
-    }
+  components: {
+    "app-server-status": ServerStatus
   }
 };
 </script>
