9155e363a7c43ac7908219b0155d6a304d2dfc92
Ajit <codemasterblackperl@outlook.com>
2018-05-21 21:39:26 +0530

    completed the directives

---
+++ src/custom-directives/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Custom Directives</h1>
+        <p v-local-highlight="'red'">Color this</p>
+        <br>
+        <p v-highlight:background.delayed="'red'">Color this</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  directives: {
+    "local-highlight": {
+      bind(el, binding, vnode) {
+        var delay = 0;
+        if (binding.modifiers["delayed"]) {
+          delay = 1000;
+        }
+        setTimeout(() => {
+          if (binding.arg == "background") {
+            el.style.backgroundColor = binding.value;
+          } else {
+            el.style.color = "green";
+          }
+        }, delay);
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,30 @@
-import Vue from 'vue';
-import App from './App.vue';
+import Vue from "vue";
+import App from "./custom-directives/App.vue";
+
+Vue.directive("highlight", {
+  bind(el, binding, vnode) {
+    //el.style.backgroundColor="green";
+    //el.style.backgroundColor = binding.value;
+    // if (binding.arg == "background") {
+    //   el.style.backgroundColor = binding.value;
+    // } else {
+    //   el.style.color = "green";
+    // }
+    var delay = 0;
+    if (binding.modifiers["delayed"]) {
+      delay = 3000;
+    }
+    setTimeout(() => {
+      if (binding.arg == "background") {
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = "green";
+      }
+    }, delay);
+  }
+});
 
 new Vue({
-  el: '#app',
+  el: "#app",
   render: h => h(App)
 });
